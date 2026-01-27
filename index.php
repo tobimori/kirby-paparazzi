15201e2d40b21b9625bbd9a483f36871d28c6793
tobimori <tobias@moeritz.io>
2026-01-27 17:15:45 +0100

    upgrade spatie/browsershot to v5, add kirby 5 support

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ use Kirby\Cms\App;
 @include_once __DIR__ . '/vendor/autoload.php';
 
 if (
-  version_compare(App::version() ?? '0.0.0', '4.0.0-beta.2', '<') === true ||
-  version_compare(App::version() ?? '0.0.0', '5.0.0', '>') === true
+  version_compare(App::version() ?? '0.0.0', '4.0.0', '<') === true ||
+  version_compare(App::version() ?? '0.0.0', '6.0.0', '>') === true
 ) {
-  throw new Exception('Kirby Paparazzi requires Kirby 4');
+  throw new Exception('Kirby Paparazzi requires Kirby 4 or 5');
 }
 
 App::plugin('tobimori/paparazzi', [
